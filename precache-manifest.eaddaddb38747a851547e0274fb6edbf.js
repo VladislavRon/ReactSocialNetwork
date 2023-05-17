8886564fb8f7c9c7b83d0a57fea62d3205884253
Vladyslav <ronenko777@gmail.com>
2023-05-17 14:02:10 +0200

    Updates

---
+++ precache-manifest.eaddaddb38747a851547e0274fb6edbf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b91f8a5eff8d67fafd67",
+    "url": "/ReactSocialNetwork/static/css/main.d42b81fd.chunk.css"
+  },
+  {
+    "revision": "b91f8a5eff8d67fafd67",
+    "url": "/ReactSocialNetwork/static/js/main.b91f8a5e.chunk.js"
+  },
+  {
+    "revision": "e10f646cde4bf01cefcc",
+    "url": "/ReactSocialNetwork/static/css/1.79ce128e.chunk.css"
+  },
+  {
+    "revision": "e10f646cde4bf01cefcc",
+    "url": "/ReactSocialNetwork/static/js/1.e10f646c.chunk.js"
+  },
+  {
+    "revision": "96924d0f6df6fe3f5672",
+    "url": "/ReactSocialNetwork/static/css/2.ba2fcd43.chunk.css"
+  },
+  {
+    "revision": "96924d0f6df6fe3f5672",
+    "url": "/ReactSocialNetwork/static/js/2.96924d0f.chunk.js"
+  },
+  {
+    "revision": "a0581bcc72877eabb362",
+    "url": "/ReactSocialNetwork/static/js/3.a0581bcc.chunk.js"
+  },
+  {
+    "revision": "51b6b32c995131082eea",
+    "url": "/ReactSocialNetwork/static/js/runtime~main.51b6b32c.js"
+  },
+  {
+    "revision": "b4b06117de3edd2f4c7f70ed369f8471",
+    "url": "/ReactSocialNetwork/static/media/logo.b4b06117.png"
+  },
+  {
+    "revision": "87ee784316298322010e3ca0edb5e519",
+    "url": "/ReactSocialNetwork/static/media/peco-pall.87ee7843.png"
+  },
+  {
+    "revision": "cfd7dc65742974ffcd7ccda9ca8a55e8",
+    "url": "/ReactSocialNetwork/static/media/loader.cfd7dc65.svg"
+  },
+  {
+    "revision": "a95a57b2d9feec13e86e60ae8fb516f0",
+    "url": "/ReactSocialNetwork/index.html"
+  }
+];
